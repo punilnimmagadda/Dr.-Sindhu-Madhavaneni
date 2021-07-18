e716fda2add473334355f8ce076b4061b35fa68c
punilnimmagadda <punilnimmagadda@gmail.com>
2021-07-18 16:43:21 -0400

    config changes

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/dr-sindhu-madhavaneni'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/dr-sindhu-madhavaneni/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
