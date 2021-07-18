ff9368b63ed07d293ebf5937ac2822141797f08b
punilnimmagadda <punilnimmagadda@gmail.com>
2021-07-18 16:58:27 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PromoBannerComponent } from '../app/components/promo-banner/promo-banner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'promo-banner', component: PromoBannerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
