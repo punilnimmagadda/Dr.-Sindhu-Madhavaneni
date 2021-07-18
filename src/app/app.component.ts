a7139874fc9d9c8f0ac550e9878b3b8fdeceff02
punilnimmagadda <30356661+punilnimmagadda@users.noreply.github.com>
2021-07-18 15:37:57 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dr-sindhu-madhavaneni';
+export class AppComponent implements OnInit{
+  toggleIconName: string = 'menu';
+  showMobileNav: boolean = false;
+
+  ngOnInit() { }
+
+  onNavToggleClick() {
+    if (this.toggleIconName.toLowerCase() === 'menu') {
+      this.toggleIconName = 'close';
+      this.showMobileNav = true;
+    } else {
+      this.toggleIconName = 'menu';
+      this.showMobileNav = false;
+    }
+  }
+
 }
